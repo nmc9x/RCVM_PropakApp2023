b3bb7ce46aa04107a5ce51cb94159482b522b56e
nmc9x <kysutudongcom@gmail.com>
2023-10-23 23:21:24 +0700

    update

--- Project/App.PVCFC-RFID/Design/XAMLViews/ucStationStatus.xaml.cs
+++ Project/App.PVCFC-RFID/Design/XAMLViews/ucStationStatus.xaml.cs
@@ -1,20 +1,6 @@
-﻿using App.PVCFC_RFID.Controller;
-using App.PVCFC_RFID.Controller.ViewModels;
+﻿using App.PVCFC_RFID.Controller.ViewModels;
 using System;
-using System.CodeDom;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace App.PVCFC_RFID.Design.XAMLViews
 {
